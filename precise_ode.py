b297f86047a34805cb860266f2b5115fd86d246d
eyucel <eyucel@server.fake>
2016-07-11 08:36:55 -0500

    trying to get integragts

--- precise_ode.py
+++ precise_ode.py
@@ -3,13 +3,14 @@ from matplotlib import pyplot as plt
 import numpy as np
 from scipy.special import beta
 
-mp.dps = 200
+
+mp.dps = 1000
 mp.pretty = True
 
 
 
 
-n = 10
+n = 4
 
 
 j = n-1
@@ -21,28 +22,40 @@ H = lambda c, w: (-(1-c)**(j-k) * c**(k-1) * (w-1) * (1-2*c + w))/(2 * mp.beta(k
 G = lambda c, w: (-(1-c)**(j-k) * c**(k-1) * (w-1) * (1-2*c + w))/(2 * beta(k, j+1-k) * (1 - mp.betainc(k, j+1-k, 0, c, regularized=True)))
 num = lambda c, w: -(1-c)**(j-k) * c**(k-1) * (w-1) * (1-2*c + w)
 denom = lambda c,w: 2 * mp.beta(k, j+1-k) * (1 - mp.betainc(k, j+1-k, 0, c, regularized=True))
+
+func = lambda c, w: (2**(-1 - n) * (2 - c)**(-k + n) * c**(-1 +  k) * (-4 + 4 * c - 2 * c * w + w**2))  \
+     / (mp.beta(k, 1 - k + n)* (-1 + mp.betainc( k, 1 - k + n,0, c/2) * (c - w)))
+
 print(H(.2,.5))
 print(G(.2,.5))
-c_end = mp.mpf('0.01')
-c_0 = mp.mpf('.999')
-w_0 = mp.mpf('.997')
+c_end = mp.mpf('0')
+c_0 = mp.mpf('1.995')
+w_0 = mp.mpf('1.99')
 
 w_c = [w_0]
 cs = [c_0]
 
-nt = 200
-incr = (c_0 - c_end)/nt
-for i in range(0,nt):
+nt = 1001
+incr = mp.mpf('0.001')
+print(incr)
+c = c_0
+i = 0
+while c > 0:
 
     w = w_c[i]
     c = cs[i]
-    n = num(c,w)
-    d = denom(c,w)
-    rhs = n/d
-    # print(rhs)
-    w_c.append(w-incr * rhs )
+    # n = num(c,w)
+    # d = denom(c,w)
+    # print(type(d))
+    # rhs = n/d
+
+    rhs=func(c,w)
+    print(rhs)
+    w_c.append(w - incr * rhs )
     cs.append(cs[i]-incr)
+    i+=1
     print(i)
+
 print(w_c)
 plt.plot(cs,w_c)
 plt.show()
